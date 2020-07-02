0e1306bc21f2a6552e3bc6092c48b22fc316f2c1
bojue <lansemaikefeng@126.com>
2020-07-02 09:54:33 +0800

    change img

---
+++ demo/index.js
@@ -0,0 +1,14 @@
+let removeGhosting = function(event) {
+    if(!(event instanceof MouseEvent)) {
+        console.info("Parameters must be of type MouseEvent!")
+        return;
+    }
+    let dragIcon = document.createElement('img');
+    dragIcon.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+    dragIcon.width = 0;
+    dragIcon.height = 0;
+    dragIcon.opacity = 0;
+    if(event && event.dataTransfer) {
+        event.dataTransfer.setDragImage(dragIcon,0, 0);
+    }
+}
